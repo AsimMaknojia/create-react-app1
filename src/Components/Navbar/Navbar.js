5453e33ce9e26e4b2a44e1e08a89db63316450ba
asim <asim.maknojia1997@gmail.com>
2023-01-10 17:18:17 +0500

    helloupdate

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { MenuItems } from './MenuItems';
+import './Navbar.css';
+class Navbar extends Component {
+    render(){
+        return(
+<div className=''>
+            <nav className='NavbarItems p-5'>
+                <div className='col-md-4 logo'>
+                <a href="http://demowebs.1stopwebsitesolution.com/dropshippinguk/"><img src={require('.//img/Group-188.png')} alt="" /></a>    
+                    {/* <h1 className='navbar-logo'>React<i className='fab fa-react'></i></h1> */}
+                </div>
+                <div className='col-md-4 menu-icon'>
+                <ul>
+                    {MenuItems?.map((item, index) => {
+                        return(
+                         <li key={index}> 
+                             <a className={item.cName} href={item.url}>
+                                {item.title}         
+                            </a>
+                        </li>
+                        )
+                    })}
+                </ul>
+                </div>
+                <div className='col-md-4 getaquote-button'>
+                <a href="https://www.google.com/"><button>Get a Quote</button></a>
+                    </div>
+            </nav>
+            </div>
+        )
+    }
+}
+
+export default Navbar
+
+
+
+
+
+
+
+
+
